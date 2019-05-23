1449b960ec68fa03f8d11ec2e89b92ab90db243d
felipedelatorre <felipede_latorre@hotmail.com>
2019-05-22 19:51:03 -0700

    done with part 1

--- edit-file.js
+++ edit-file.js
@@ -1,13 +1,34 @@
 'use strict';
-
-
-
 const fs = require('fs');
+const faker = require('faker');
 
 
-let editFile = (fileName) => {
-  fs.readFile('/etc/passwd', (err, data) => {
+let fileName = process.argv[2];
+
+let readAndWrite = (file, cb) => {
+  let fakeInfo = faker.name.findName();
+
+  console.log(`Reading ${file}`);
+  cb(file);
+  setTimeout(() => {
+    writingFile(fakeInfo, cb(file));
+  },500);
+};
+
+
+let readingFile = (fileName) => {
+  fs.readFile(`files/${fileName}`, 'utf8',(err, data) => {
     if (err) throw err;
-    console.log(data);
+    console.log('Reading inside the file: ', data);
   });
 };
+
+let writingFile = (newText, cb) => {
+  fs.writeFile('files/test.txt', `${newText}`, (err) => {
+    if (err) throw err;
+    console.log('Writing a new name');
+    cb;
+  });
+};
+
+readAndWrite(fileName, readingFile);
